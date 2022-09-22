a68a845fb3752b0170fd31cc072e844a8fc9e971
Joseph <joffan98@gmail.com>
2022-09-21 20:02:45 -0500

    commit tree 84ddef53577b759abc69d0be6419f316a6fdd323

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const countDown = number => {
+    while (number >= 0) console.log(number--)
+}
+
+const writeCards = (array,occasion) => {
+    const thankYouString = []
+    for (let name of array) {
+        thankYouString.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`)
+    }
+    return thankYouString
+}
